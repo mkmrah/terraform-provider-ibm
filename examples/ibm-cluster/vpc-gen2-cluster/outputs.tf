2fc7e3201ac05a45c9728d508c1e940a486e74fc
mkmrah <39188713+mkmrah@users.noreply.github.com>
2021-11-11 12:15:15 +0530

    Update outputs.tf

--- examples/ibm-cluster/vpc-gen2-cluster/outputs.tf
+++ examples/ibm-cluster/vpc-gen2-cluster/outputs.tf
@@ -3,3 +3,9 @@ output "cluster_config_file_path" {
   value = data.ibm_container_cluster_config.cluster_config.config_file_path
 }
 
+
+output cluster_id {
+    description = "Cluster ID, will await multizone attachment before returning"
+    value       = ibm_container_vpc_cluster.cluster.id
+}
+
